0efc7857dc39eb63c5dadfe65291a11f059060d8
sermoa <aimee@mychores.co.uk>
2008-11-30 17:55:12 +0000

    Made some changes to the Capistrano deployment file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,14 @@ set :application, 'mychores'
 set :user, 'mychore'
 set :domain, 'wycliffe'
 
+
+set :scm_passphrase,  Proc.new { Capistrano::CLI.password_prompt('Git Password: ') }
+
 set :server_hostname, 'mychores.co.uk'
 
 role :web, server_hostname
 role :app, server_hostname
-role :db, server_hostname
+role :db, server_hostname, :primary => true
 
 default_run_options[:pty] = true
 set :repository,  "git@github.com:sermoa/#{application}.git"
@@ -18,5 +21,6 @@ set :branch, "master"
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
+set :use_sudo, false
 set :deploy_to, "/home/#{user}/#{application}"
 
