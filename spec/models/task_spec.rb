f64f0d64c7ed17903e0c9b58505973e633c949a1
Matthew Sadler <mat@Thursday.local>
2008-11-30 11:53:59 +0000

    Tests for Task#describe_recurrence.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -35,4 +35,47 @@ describe Task do
       task.short_name.should == task.name
     end
   end
+  
+  describe "describe_recurrence" do
+    it "should describe a one off task" do
+      task = Task.new(:one_off => true)
+      task.describe_recurrence
+      task.recurrence_description.should == "One-off task"
+    end
+    
+    it "should describe an every other day task" do
+      task = Task.new(:recurrence_measure => "days", :recurrence_interval => 2)
+      task.describe_recurrence
+      task.recurrence_description.should == "Every other day"
+    end
+    
+    it "should describe a once a week task" do
+      task = Task.new(:recurrence_measure => "weeks", :recurrence_interval => 1)
+      task.describe_recurrence
+      task.recurrence_description.should == "Every week"
+    end
+    
+    it "should describe a once a week on tuesdays task" do
+      task = Task.new(:recurrence_measure => "weeks", :recurrence_interval => 1,
+                      :recurrence_occur_on => %W{2})
+      task.describe_recurrence
+      task.recurrence_description.should == "Every week (Tuesday)"
+    end
+    
+    it "should describe a once a week not on tuesdays task" do
+      task = Task.new(:recurrence_measure => "weeks", :recurrence_interval => 1,
+                      :recurrence_occur_on => %W{0 1 3 4 5 6})
+      task.save
+      task.describe_recurrence
+      task.recurrence_description.should == "Every week (not Tuesday)"
+    end
+    
+    it "should describe a every 3 months task" do
+      task = Task.new(:recurrence_measure => "months",
+                      :recurrence_interval => 3)
+      task.describe_recurrence
+      task.recurrence_description.should == "Every 3 months"
+    end
+  end
+
 end
