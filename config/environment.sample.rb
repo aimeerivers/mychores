3f989b8d6f51edeefe1c64d5d8b1fb5dc9726d33
sermoa <aimee@mychores.co.uk>
2009-04-08 18:40:54 +0100

    Added ruby-openid as a frozen gem.

--- config/environment.sample.rb
+++ config/environment.sample.rb
@@ -4,67 +4,25 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  # See Rails::Configuration for more options.
+  config.gem 'recaptcha'
+  config.gem 'RedCloth'
+  config.gem 'ruby-openid', :lib => 'openid'
 
-  # Skip frameworks you're not going to use (only works if using vendor/rails).
-  # To use Rails without a database, you must remove the Active Record framework
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Only load the plugins named here, in the order given. By default, all plugins 
-  # in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
-
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
-  # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => 'invent_something',
     :secret      => 'blah_de_blah_put_your_own'
   }
-
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with 'rake db:sessions:create')
   config.action_controller.session_store = :active_record_store
-
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector
 
   config.time_zone = 'London'
 end
 
-
-
 DAYSOFWEEK = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
 
 AVAILABLEDAYS = [ "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th" ]
-
-
-
 
 # ActionMailer settings:
 ActionMailer::Base.delivery_method = :smtp
@@ -77,14 +35,9 @@ ActionMailer::Base.smtp_settings = {
 	:authentication  => :login
 }
 
-
-
-
 CGI::Session.expire_after 1.month
 
 require 'will_paginate'
-
-
 
 RCC_PUB = 'your_public_key_for_recaptcha'.freeze
 RCC_PRIV = 'your_private_key_for_recaptcha'.freeze
